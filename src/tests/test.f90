bbe19bbfab9d7369407646adcb2d5e246f206c71
Jacob Williams <jacobwilliams@users.noreply.github.com>
2015-12-27 00:10:46 -0600

    minor commenting updates

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -1,17 +1,15 @@
 !*****************************************************************************************
 !>
-!  Test program for [[ddeabm_class]].
-!  Integrate a two-body orbit around the Earth.
+!  Test program for ddeabm.
 
     program test
 
-    use ddeabm_module
-    use root_module
+    use ddeabm_module, only: ddeabm_test
+    use root_module,   only: zeroin_test
 
     implicit none
 
     call zeroin_test()
-
     call ddeabm_test()
 
     end program test
