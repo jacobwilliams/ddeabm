61f59e913a630948343e95b6f2a93a880e1d1694
TRAVIS-CI-for-GitHub <noreply@github.com>
2018-01-01 20:43:12 +0000

    Travis CI autocommit from Travis job 31.1 for tag  901e3ecf9f0703de1282a4a47e5cb3fb7e14d91d

--- src/root_module.f90
+++ src/root_module.f90
@@ -1,6 +1,7 @@
 !*****************************************************************************************
 !> author: Jacob Williams
 !  date: 12/22/2015
+!  license: BSD
 !
 !  Root-finding routines. Used by [[ddeabm_module]].
 
