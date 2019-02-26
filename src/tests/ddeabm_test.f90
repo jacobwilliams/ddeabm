af199de9672f4ca5a11ea5f54447b5f184b01b1a
Jacob Williams <jacobwilliams@users.noreply.github.com>
2019-02-25 22:03:13 -0600

    Adding custom bracket function and continue function. See #14 and #14. Something still not quite right.

--- src/tests/ddeabm_test.f90
+++ src/tests/ddeabm_test.f90
@@ -13,21 +13,31 @@
 
     implicit none
 
+    integer,parameter  :: n = 6            !! number of state variables
+    real(wp),parameter :: tol = 1.0e-12_wp !! event location tolerance
+    real(wp),parameter :: rtol = 1.0e-12_wp !! integrator tolerances
+    real(wp),parameter :: atol = 1.0e-12_wp !! integrator tolerances
+    integer,parameter :: maxnum = 10000  !! max func evals
+    real(wp),parameter :: mu = 398600.436233_wp !! MU for Earth
+
+    real(wp),dimension(6),parameter :: x0 = [10000.0_wp,10000.0_wp,10000.0_wp,&   !! initial state [r,v] (km,km/s)
+                                             1.0_wp,2.0_wp,3.0_wp]
+    real(wp),parameter :: t0 = 0.0_wp       !! initial time (sec)
+
+
     type,extends(ddeabm_with_event_class) :: spacecraft
         !! spacecraft propagation type.
         !! extends the [[ddeabm_class]] to include data used in the deriv routine
-        real(wp) :: mu = 0.0_wp      !! central body gravitational parameter (km3/s2)
+        real(wp) :: mu = mu          !! central body gravitational parameter (km3/s2)
         integer :: fevals = 0        !! number of function evaluations
         logical :: first = .true.    !! first point is being exported
     end type spacecraft
 
-    integer,parameter  :: n = 6            !! number of state variables
-    real(wp),parameter :: tol = 1.0e-12_wp !! event location tolerance
-
-    type(spacecraft) :: s
-    real(wp),dimension(n) :: x0,xf,x02,x
-    real(wp) :: t0,tf,dt,gf,tf_actual,t,gval,z_target
+    type(spacecraft) :: s, s2
+    real(wp),dimension(n) :: xf,x02,x
+    real(wp) :: tf,dt,gf,tf_actual,t,gval,z_target
     integer :: idid
+    integer :: icase
 
     write(*,*) ''
     write(*,*) '---------------'
@@ -45,14 +55,13 @@
 
     !constructor (main body is Earth):
 
-    call s%initialize(n,maxnum=10000,df=twobody,rtol=[1.0e-12_wp],atol=[1.0e-12_wp],&
+    call s%initialize(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol],&
                         report=twobody_report)
-    s%mu = 398600.436233_wp  !earth
 
     !initial conditions:
-    x0 = [10000.0_wp,10000.0_wp,10000.0_wp,&   !initial state [r,v] (km,km/s)
-            1.0_wp,2.0_wp,3.0_wp]
-    t0 = 0.0_wp       !initial time (sec)
+    ! x0 = [10000.0_wp,10000.0_wp,10000.0_wp,&   !initial state [r,v] (km,km/s)
+    !         1.0_wp,2.0_wp,3.0_wp]
+    ! t0 = 0.0_wp       !initial time (sec)
     tf = 1000.0_wp    !final time (sec)
     s%fevals = 0
 
@@ -115,15 +124,14 @@
     write(*,*) '-----------------------'
     write(*,*) ''
 
-    call s%initialize_event(n,maxnum=10000,df=twobody,rtol=[1.0e-12_wp],atol=[1.0e-12_wp],&
-                                g=twobody_event,root_tol=1.0e-12_wp,report=twobody_report)
-    s%mu = 398600.436233_wp  !earth
-    s%fevals = 0
+    call s%destroy()
+    call s%initialize_event(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol],&
+                                g=twobody_event,root_tol=tol,report=twobody_report)
 
     !initial conditions:
-    x0 = [10000.0_wp,10000.0_wp,10000.0_wp,&   !initial state [r,v] (km,km/s)
-            1.0_wp,2.0_wp,3.0_wp]
-    t0 = 0.0_wp       !initial time (sec)
+    ! x0 = [10000.0_wp,10000.0_wp,10000.0_wp,&   !initial state [r,v] (km,km/s)
+    !         1.0_wp,2.0_wp,3.0_wp]
+    ! t0 = 0.0_wp       !initial time (sec)
 
     write(*,'(A/,*(F15.6/))') 'Initial time:',t0
     write(*,'(A/,*(F15.6/))') 'Initial state:',x0
@@ -138,11 +146,49 @@
     xf = x
     write(*,*) ''
     write(*,'(A/,*(I5/))')    'idid: ',idid
-    write(*,'(A/,*(F15.6/))') 'gval: ',gval
+    write(*,'(A/,*(F30.16/))') 'gval: ',gval
     write(*,'(A/,*(F15.6/))') 'Final time:',t
     write(*,'(A/,*(F15.6/))') 'Final state:',xf
     write(*,'(A,I5)') 'Function evaluations:', s%fevals
     write(*,*) ''
+
+    ! ! .................... this works fine when not continuing .........
+    ! write(*,*) '======================='
+    ! write(*,*) 'integrate normally to root and compare ...'
+    ! write(*,*) '======================='
+    ! call s%first_call()  !have to restart the integration after a root finding
+    ! tf = t
+    ! t = t0
+    ! x = x0
+    ! call s%integrate(t,x,tf,idid=idid,integration_mode=2)
+    ! call twobody_event(s,t,x,gval)
+    ! write(*,*) ''
+    ! write(*,'(A/,*(I5/))')    'idid: ',idid
+    ! write(*,'(A/,*(F30.16/))') 'gval: ',gval
+    ! write(*,'(A/,*(F15.6/))') 'Final time:',t
+    ! write(*,'(A/,*(F15.6/))') 'Final state:',x
+    ! write(*,*) ''
+
+    ! write(*,*) '======================='
+    ! write(*,*) 'integrate with fixed step size to root and compare ...'
+    ! write(*,*) '======================='
+    ! call s%first_call()  !have to restart the integration after a root finding
+    ! tf = t
+    ! t = t0
+    ! x = x0
+    ! call s%integrate(t,x,tf,idid=idid,integration_mode=2,tstep=50.0_wp)
+    ! call twobody_event(s,t,x,gval)
+    ! write(*,*) ''
+    ! write(*,'(A/,*(I5/))')    'idid: ',idid
+    ! write(*,'(A/,*(F30.16/))') 'gval: ',gval
+    ! write(*,'(A/,*(F15.6/))') 'Final time:',t
+    ! write(*,'(A/,*(F15.6/))') 'Final state:',x
+    ! write(*,*) ''
+
+    ! stop
+    ! ! ....................
+
+    !--------------------------------------------------------------------------------------
 
     ! Now, continue integration (until z-coordinate is 13,000 km)
 
@@ -150,25 +196,172 @@
     write(*,*) 'continue integration...'
     write(*,*) '======================='
 
-    s%mu = 398600.436233_wp  !earth
     s%fevals = 0
     s%first = .true.
     x = xf
     tf = 2000.0_wp    !max final time (sec)
     z_target = 13000.0_wp !change the target value
-    write(*,'(A/,*(F15.6/))') 'Initial time     :',t
-    write(*,'(A/,*(F15.6/))') 'Max final time   :',tf
-    write(*,'(A/,*(F15.6/))') 'Initial state    :',x
-    call s%first_call()  !have to restart the integration after a root finding
-    call s%integrate_to_event(t,x,tf,idid=idid,gval=gval,integration_mode=2,tstep=50.0_wp) ! test dense output here
+    ! write(*,'(A/,*(F15.6/))') 'Initial time     :',t
+    ! write(*,'(A/,*(F15.6/))') 'Max final time   :',tf
+    ! write(*,'(A/,*(F15.6/))') 'Initial state    :',x
+
+    ! Some issue here or with the ddeabm code.
+    ! When using a fixed-step output, the result isn't as accurate
+    ! (when continuing it much less accurate than when restarting the integration)
+    ! Not sure why that would be... the root should be the result of the same interpolation
+
+    !...new ... try to continue without restarting ...
+    call s%integrate_to_event(t,x,tf,idid=idid,gval=gval,integration_mode=2,tstep=50.0_wp,continue=.true.) ! test dense output here  -- this is not accurate at all!
+    !call s%integrate_to_event(t,x,tf,idid=idid,gval=gval,integration_mode=2,continue=.true.) ! default points  -- this is accurate
+
+    !...original... restart integration ...
+    ! call s%first_call()  !have to restart the integration after a root finding
+    ! call s%integrate_to_event(t,x,tf,idid=idid,gval=gval,integration_mode=2,tstep=50.0_wp) ! test dense output here -- this is OK but not super accurate
+    ! !call s%integrate_to_event(t,x,tf,idid=idid,gval=gval,integration_mode=2) ! default points  -- this is accurate
+
     xf = x
     write(*,*) ''
     write(*,'(A/,*(I5/))')    'idid: ',idid
-    write(*,'(A/,*(F15.6/))') 'gval: ',gval
+    write(*,'(A/,*(F30.16/))') 'gval: ',gval
     write(*,'(A/,*(F15.6/))') 'Final time:',t
     write(*,'(A/,*(F15.6/))') 'Final state:',xf
     write(*,'(A,I5)') 'Function evaluations:', s%fevals
     write(*,*) ''
+
+    write(*,*) '======================='
+    write(*,*) 'integrate normally to root and compare ...'
+    write(*,*) '======================='
+
+    call s2%initialize(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol])
+    call s2%first_call()  !have to restart the integration after a root finding
+    s2%fevals = 0
+    tf = t
+    t = t0
+    x = x0
+    call s2%integrate(t,x,tf,idid=idid,integration_mode=1)
+    call twobody_event(s2,t,x,gval)
+    write(*,*) ''
+    write(*,'(A/,*(I5/))')     'idid: ',idid
+    write(*,'(A/,*(F30.16/))') 'gval: ',gval
+    write(*,'(A/,*(F15.6/))')  'Initial time:',t0
+    write(*,'(A/,*(F15.6/))')  'Final time:',t
+    write(*,'(A/,*(F15.6/))')  'Final state:',x
+    write(*,'(A,I5)') 'Function evaluations:', s2%fevals
+    write(*,*) ''
+
+    write(*,*) '======================='
+    write(*,*) 'integrate with fixed step size to root and compare ...'
+    write(*,*) '======================='
+    call s2%initialize(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol])
+    call s2%first_call()  !have to restart the integration after a root finding
+    s2%fevals = 0
+    tf = t
+    t = t0
+    x = x0
+    call s2%integrate(t,x,tf,idid=idid,integration_mode=1,tstep=50.0_wp)
+    call twobody_event(s2,t,x,gval)
+    write(*,*) ''
+    write(*,'(A/,*(I5/))')     'idid: ',idid
+    write(*,'(A/,*(F30.16/))') 'gval: ',gval
+    write(*,'(A/,*(F15.6/))')  'Initial time:',t0
+    write(*,'(A/,*(F15.6/))')  'Final time:',t
+    write(*,'(A/,*(F15.6/))')  'Final state:',x
+    write(*,'(A,I5)') 'Function evaluations:', s2%fevals
+    write(*,*) ''
+
+    !****************************************************************************************
+
+    !
+    ! try continuing without root finding. This accurate for both default and fixed steps
+    !
+
+    do icase = 1, 2  !first with default steps, next with fixed steps
+
+        write(*,*) ''
+        write(*,*) '========================================================='
+        if (icase==1) then
+            write(*,*) ' default steps'
+        else
+            write(*,*) ' fixed 50 sec step'
+        end if
+        write(*,*) '========================================================='
+        write(*,*) ''
+
+        write(*,*) ''
+        write(*,*) '====================================='
+        write(*,*) ' integrate from t=0 to t=300'
+
+        call s%initialize(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol],&
+                            report=twobody_report)
+
+        !initial conditions:
+        tf = 300.0_wp    !final time (sec)
+        s%fevals = 0
+        s%fevals = 0
+        s%first = .true.
+        t = t0
+        x = x0
+        call s%first_call()
+        if (icase==1) then
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1)
+        else
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1,tstep=50.0_wp)
+        end if
+        xf = x
+        write(*,*) ''
+        write(*,'(A/,*(I5/))')     'idid: ',idid
+        write(*,'(A/,*(F15.6/))')  'Initial time:',t0
+        write(*,'(A/,*(F15.6/))')  'Final time:',t
+        write(*,'(A/,*(F15.6/))')  'Final state:',x
+        write(*,'(A,I5)') 'Function evaluations:', s%fevals
+        write(*,*) ''
+
+        write(*,*) ' continue to t=600'
+        t = tf
+        tf = 600.0_wp
+        if (icase==1) then
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1)
+        else
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1,tstep=50.0_wp)
+        end if
+        write(*,*) ''
+        write(*,'(A/,*(I5/))')     'idid: ',idid
+        write(*,'(A/,*(F15.6/))')  'Final time:',t
+        write(*,'(A/,*(F15.6/))')  'Final state:',x
+        write(*,'(A,I5)') 'Function evaluations:', s%fevals
+        write(*,*) ''
+
+        write(*,*) ''
+        write(*,*) '====================================='
+        write(*,*) ' integrate all at once from t=0 to t=600'
+
+        call s%initialize(n,maxnum=maxnum,df=twobody,rtol=[rtol],atol=[atol],&
+                            report=twobody_report)
+        !initial conditions:
+        tf = 600.0_wp    !final time (sec)
+        s%fevals = 0
+        s%fevals = 0
+        s%first = .true.
+        t = t0
+        x = x0
+        call s%first_call()
+        if (icase==1) then
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1)
+        else
+            call s%integrate(t,x,tf,idid=idid,integration_mode=1,tstep=50.0_wp)
+        end if
+        xf = x
+        write(*,*) ''
+        write(*,'(A/,*(I5/))')     'idid: ',idid
+        write(*,'(A/,*(F15.6/))')  'Initial time:',t0
+        write(*,'(A/,*(F15.6/))')  'Final time:',t
+        write(*,'(A/,*(F15.6/))')  'Final state:',x
+        write(*,'(A,I5)') 'Function evaluations:', s%fevals
+        write(*,*) ''
+
+        write(*,*) '====================================='
+
+    end do
 
     contains
 !*****************************************************************************************
